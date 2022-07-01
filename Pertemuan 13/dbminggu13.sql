6d45845a7fbeb6e7a902e3f11a91c5a20358c066
debiapriani22 <105537067+debiapriani22@users.noreply.github.com>
2022-07-01 14:34:31 +0700

    Add files via upload

---
+++ Pertemuan 13/dbminggu13.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 18, 2022 at 08:10 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbminggu13`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bljr_login`
+--
+
+CREATE TABLE `bljr_login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bljr_login`
+--
+
+INSERT INTO `bljr_login` (`id`, `username`, `password`) VALUES
+(1, 'Andi', 'uhuy123'),
+(2, 'Santoso', 'qwerty'),
+(3, 'Samsul', 'dodopret'),
+(4, 'Administrator', 'admin123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
